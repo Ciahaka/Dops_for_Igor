ebeb161306707773f8c6cfe17cb1c83d51604426
Ciahaka <wilddiver.by@gmail.com>
2022-11-16 02:02:18 +0300

    add to heroku commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,50 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Button} from './components/button/Button';
 
+type ShowPropsType = {
+  completed: boolean
+  id: number
+  title: string
+  userId: number
+}
+
 function App() {
+  const [showJson, setShowJson] = useState<ShowPropsType[]>([])
 
-  const [showJson,setShowJson] = useState([])
 
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-      .then(response => response.json())
-      .then(json => setShowJson(json))
-  }, [])
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/todos')
+  //     .then(response => response.json())
+  //     .then(json => setShowJson(json))
+  // }, [])
 
-  const showMeJson  = ()=>{
-    console.log(showJson)
+  const useShowMeJson = () => {
+    // useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(response => response.json())
+        .then(json => setShowJson(json))
+    // }, [])
   }
+
+  const rebirthPage = () =>{
+    setShowJson([])
+  };
 
   return (
     <div className="App">
-      <Button name={'KliK'} callback={showMeJson}/>
+
+      <Button name={'KliK'} callback={useShowMeJson}/>
+      <Button name={'RebirtH'} callback={rebirthPage}/>
+
+      <ul>
+        {showJson.map(el => {
+          return (
+            <li key={el.id}>
+              <span>{`${el.id} ${el.title} ${el.userId} ${el.completed} `}</span>
+            </li>
+          )
+        })}
+      </ul>
     </div>
   );
 }
