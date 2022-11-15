e5b709acc3bdd34a73555c4ee888795f795c9246
Ciahaka <wilddiver.by@gmail.com>
2022-11-15 23:45:13 +0300

    add component_Button_to_App

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
+
 import './App.css';
+import {Button} from './components/button/Button';
 
 function App() {
+
+  const [showJson,setShowJson] = useState([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(json => setShowJson(json))
+  }, [])
+
+  const showMeJson  = ()=>{
+    console.log(showJson)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button name={'KliK'} callback={showMeJson}/>
     </div>
   );
 }
